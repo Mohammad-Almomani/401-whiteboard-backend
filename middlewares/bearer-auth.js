1b3e23b82fbe6ce71bb4211bf3e6f185ca437140
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-10-04 15:10:43 +0300

    created new profile end point

--- middlewares/bearer-auth.js
+++ middlewares/bearer-auth.js
@@ -3,7 +3,8 @@
 const { Users } = require("../models");
 /* istanbul ignore next */
 const bearerCheck = async (req, res, next) => {
-  if (!req.headers.authorization) return res.send("Invalid Login");
+
+  if (!req.headers.authorization) return res.status(401).send("Invalid Login");
   console.log("Bearer middleware");
   const token = req.headers.authorization.split(" ").pop();
   try {
@@ -16,7 +17,6 @@ const bearerCheck = async (req, res, next) => {
       req.user = userInfo;
       req.body.username = userInfo.username;
       req.token = userInfo.token;
-
       next();
     } else {
       return res.send("Invalid Login");
